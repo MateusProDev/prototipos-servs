b74b93b7e22d4f53180d9cca409964a293c17b4f
MateusProDev <mateusferreiraprodev@gmail.com>
2024-01-21 12:01:04 -0300

    comprimido fundo do site

--- portifolio-mfs/src/App.jsx
+++ portifolio-mfs/src/App.jsx
@@ -2,18 +2,12 @@ import './App.css'
 import HeaderNav from './components/HeaderNav.jsx'
 import MainContainer from './components/MainContainer.jsx'
 import FooterContainer from './components/FooterContainer.jsx'
-import MyForms from './components/MyForms.jsx'
 
 function App() {
 
   return (
     <>
       <HeaderNav />
-      <div>
-      <h1>Meu Portfólio</h1>
-      <MyForms />
-      {/* Outros componentes do seu portfólio */}
-      </div>
       <MainContainer />
       <FooterContainer />
     </>
